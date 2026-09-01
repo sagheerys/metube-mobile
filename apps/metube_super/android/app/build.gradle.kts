7cfc3fde938b18b596595ad4fd2db03ed864a96e
Yasir Sagheer <226705953+sagheerys@users.noreply.github.com>
2026-09-01 06:11:11 +0300

    checkpoint: تلقائي قبل تعديل (2026-09-01 06:11)

--- apps/metube_super/android/app/build.gradle.kts
+++ apps/metube_super/android/app/build.gradle.kts
@@ -26,7 +26,7 @@ android {
 
     defaultConfig {
         applicationId = "com.yasir.metubesuper"
-        minSdk = 23
+        minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
